438ff53110fb50ff2b42e85ed6a5cefca549f7ff
Nicolas Pettican <nicolaspettican@gmail.com>
2026-05-18 21:44:58 +0530

    Add GA4 tracking, update footer

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-9DKZ2SYTQK",
     },
     locale: "en-US",
     baseUrl: "pramana.middlewaymusings.com",
